85e13e271b59d1c10cb79b19773cc9aa435360d3
Roman <roma.ne4aev2013@gmail.com>
2020-03-19 19:57:50 +0200

    add car class

--- CarStorage/CarStorage/Program.cs
+++ CarStorage/CarStorage/Program.cs
@@ -6,7 +6,23 @@ namespace CarStorage
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car[] cars = new Car[3];
+
+            for (var i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine("Please input car's name");
+                cars[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Please input car's name");
+                cars[i].Price = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input color");
+                cars[i].Color = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Source price: {cars[0].Price};Price after sale:{cars[0].GetSale()}");
+        
         }
     }
 }
